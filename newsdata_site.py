1621be7b3063dd41d278525b6ebd113a1999a0a3
benaronberg <benberc@gmail.com>
2019-05-05 19:19:01 -0400

    Changes to comply with pycodestyle

--- newsdata_site.py
+++ newsdata_site.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import psycopg2
 from flask import Flask, render_template, Markup
 
@@ -19,23 +20,29 @@ def main():
 @app.route('/top_3', methods=['GET'])
 def top_3():
     """Return the most popular three articles of all time in the database"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select articles.title, count(log.id) as views from articles, log where log.path like '%' || articles.slug || '%' group by log.path, articles.title order by views desc limit 3;")
-    r = c.fetchall()
-    db.close()
+    query = '''SELECT articles.title, count(log.id) as views
+                    FROM articles, log
+                    WHERE log.path like '%' || articles.slug || '%'
+                    GROUP BY log.path, articles.title
+                    ORDER BY views desc limit 3;
+            '''
+    r = execute_query(query)
     result = "".join(RESULT % (title, views) for title, views in r)
     return render_template('HTML_WRAP.html', result=result)
 
 
 @app.route('/authors_by_popularity', methods=['GET'])
 def authors_by_popularity():
-    """Return all article authors and the number of page views articles written by each auther have recieved, sorted from most views to least views"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute("select authors.name, count(log.id) as views from authors, articles, log where log.path like '%' || articles.slug || '%' and articles.author = authors.id group by authors.id order by views desc;")
-    r = c.fetchall()
-    db.close()
+    """Return all article authors and the number of page views articles written
+        by each auther have recieved, sorted from most views to least views"""
+    query = '''SELECT authors.name, count(log.id) as views
+                    FROM authors, articles, log
+                    WHERE log.path like '%' || articles.slug || '%'
+                    AND articles.author = authors.id
+                    GROUP BY authors.id
+                    ORDER BY views desc;
+            '''
+    r = execute_query(query)
     result = "".join(RESULT % (name, views) for name, views in r)
     return render_template('HTML_WRAP.html', result=result)
 
@@ -43,13 +50,32 @@ def authors_by_popularity():
 @app.route('/peak_error_days', methods=['GET'])
 def peak_error_days():
     """Return days on which more than 1% of requests led%to errors"""
+    query = '''SELECT date(time),
+                    cast(100 * cast(sum(
+                    case when status not like '%200%' then 1 else 0 end)
+                    as decimal(7,2)) /
+                    cast(count(id) as decimal(7,2)) as decimal(7,2))
+                    as percent_errors
+                    FROM log
+                    GROUP BY date(time)
+                    having cast(sum(case when
+                    status not like '%200%' then 1 else 0 end)
+                    as decimal(7,2)) / cast(count(id) as decimal(7,2)) >= 0.01;
+            '''
+
+    r = execute_query(query)
+    r = '''RESULT % (time, percent_errors) for time, percent_errors in r'''
+    result = "".join(result_join)
+    return render_template('HTML_WRAP.html', result=result)
+
+
+def execute_query(query):
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select date(time), cast(100 * cast(sum(case when status not like '%200%' then 1 else 0 end) as decimal(7,2)) / cast(count(id) as decimal(7,2)) as decimal(7,2)) as percent_errors from log group by date(time) having cast(sum(case when status not like '%200%' then 1 else 0 end) as decimal(7,2)) / cast(count(id) as decimal(7,2)) >= 0.01;")
+    c.execute(query)
     r = c.fetchall()
     db.close()
-    result = "".join(RESULT % (time, percent_errors) for time, percent_errors in r)
-    return render_template('HTML_WRAP.html', result=result)
+    return r
 
 
 if __name__ == '__main__':
